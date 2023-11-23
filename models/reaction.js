b8289636015ac27806d8df8d79e8f02c22585836
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-22 22:51:43 -0500

    add reaction model, username will be accepted as string, reaction body will be maximum length of 280 characters and string, created date is formatted to readable display

---
+++ models/reaction.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+reactionSchema.virtual('formatDate').get(function () {
+    return this.createdAt.toLocaleString(); // readable date for users 
+  });
+
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
