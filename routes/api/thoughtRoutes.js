4564caede58cd52a3bbcb0816726386bff8c3070
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-23 00:26:37 -0500

    add routes to thought routes to use crud names in thought route file to assign correct url name for each.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
-
+const {
+    getThoughts,  //thought routes file
+    getOneThought,
+    createThought,
+  } = require('../../controllers/thoughtController');
+  
+  router.route('/').get(getThoughts).post(createThought);  //routes will use the name of crud function when called
+  
+  router.route('/:thoughtId').get(getOneThought); //accepts thought id parameter in url
+  
 
 
 
