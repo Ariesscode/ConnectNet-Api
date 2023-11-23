93c560aacc531c400de248cfa7d95e6e12ff3228
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-23 01:54:36 -0500

    update route added to thought route and delete method added to user

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,13 +5,14 @@ const {
     getOneThought,
     createThought,
     removeThought,
+    updateThought,
   } = require('../../controllers/thoughtController');
   //api/thoughts
   router.route('/').get(getThoughts).post(createThought);  //routes will use the name of crud function when called
   //api/thoughts/:thoughtId
   router.route('/:thoughtId').get(getOneThought).delete(removeThought); //accepts thought id parameter in url
   
-
+router.route('/:thoughtId/update').post(updateThought);
 
 
 module.exports = router;
