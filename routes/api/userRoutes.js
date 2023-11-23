ae4d40102c774d45e07c3fcd08de82ce771139fb
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-23 02:03:23 -0500

    add function to remove a thought from user and add route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,13 +7,14 @@ const {
   createUser,
   deleteUserById,
   updateUser,
+  removeUserThought,
 } = require('../../controllers/userController'); //require the names in 
 
 // /api/users
-router.route('/').get(getAllUsers).post(createUser); 
+router.route('/').get(getAllUsers).post(createUser);  //get all users and create a user
 
 // /api/users/:userId
-router.route('/:userId').get(getOneUser).delete(deleteUserById);
-router.route('/:userId/update/').post(updateUser);
-
+router.route('/:userId').get(getOneUser).delete(deleteUserById); //get and delete user 
+router.route('/:userId/update/').post(updateUser);   //update a user
+router.route('/:userId/thought/:thoughtId').delete(removeUserThought);  //remove a thought from user
 module.exports = router;
