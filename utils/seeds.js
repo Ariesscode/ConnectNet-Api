9b793a1e7f5f62cec3d39adb4df3546d3a815538
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-23 22:25:46 -0500

    add seeds for thoughts for db

---
+++ utils/seeds.js
@@ -0,0 +1,48 @@
+const seedThoughts = [
+    {
+        text: 'This is a thought.',
+        username: 'Jennifer1',
+        reactions: [
+          {
+            reactionBody: 'This is a reaction.',
+            username: 'StanleyLewis'
+          },
+        ],
+      },
+      
+    {
+      text: 'Another thought.',
+      username: 'Hannah345',
+      reactions: []
+    },
+    {
+        text: 'Thought here.',
+        username: 'StanleyLewis',
+        reactions: [
+          {
+            reactionBody: 'Reaction here.',
+            username: 'Hannah345'
+          },
+        ],
+      },
+      {
+        text: 'Im thinking right now of a thought.',
+        username: 'chrisLui',
+        reactions: [
+          {
+            reactionBody: 'This is a reaction, cool!.',
+            username: 'Pokemon11'
+          },
+        ],
+      },
+      {
+        text: 'Thought here.',
+        username: 'Pokemon11',
+        reactions: [
+          {
+            reactionBody: 'Reaction here.',
+            username: 'Hannah345'
+          },
+        ],
+      },
+  ];
